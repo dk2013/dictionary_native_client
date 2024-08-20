a398af7dcb05bdd5a3ab3720a9dc19d290cd6970
dmuser <dmitriy.korytnyi@gmail.com>
2024-08-19 17:38:41 -0700

    corrected files / patches

--- src/Components/Dictionary/AddWord/AddWordScreen.tsx
+++ src/Components/Dictionary/AddWord/AddWordScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import PageTemplate from 'PageTemplate';
+import PageTemplate from '@components/Common/PageTemplate';
 
 const AddWord = () => {
   return (
